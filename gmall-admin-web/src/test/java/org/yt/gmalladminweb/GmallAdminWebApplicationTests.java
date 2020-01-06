90675fdc202362a414c0acfb8852949def78ef93
trickMin <2416109044@qq.com>
2020-01-06 14:06:27 +0800

    gmall项目搭建

---
+++ gmall-admin-web/src/test/java/org/yt/gmalladminweb/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yt.gmalladminweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
